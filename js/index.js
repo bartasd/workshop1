128e107b1ba434b547ad013403f5de6bb0658753
Bartas Dausynas <bartas3@gmail.com>
2023-10-26 13:26:17 +0300

    added colours

--- js/index.js
+++ js/index.js
@@ -3,23 +3,41 @@ let res = "INITIAL NUMBER"
 const sub = document.getElementById('s');
 const r = document.getElementById('r');
 const add = document.getElementById('a');
-const result = document.getElementById("res");
+const resul = document.getElementById("res");
+
+function change(res)
+{
+    if (res == "INITIAL NUMBER")
+    {
+        if(result.classList.contains('minus'))
+            resul.classList.remove('minus');
+        if(result.classList.contains('plus'))
+            resul.classList.remove('plus');
+    }
+    else if(res > 0)
+        resul.classList.add('plus');
+    else if(res < 0)
+        resul.classList.add('minus');
+}
 
 sub.addEventListener('click', function onClick() {
     if(res === "INITIAL NUMBER")
         res = 0;
     res--;
-    result.innerHTML = res;
+    change(res);
+    resul.innerHTML = res;
 });
 
 r.addEventListener('click', function onClick() {
     res = "INITIAL NUMBER";
-    result.innerHTML = res;
+    change(res);
+    resul.innerHTML = res;
 });
 
 add.addEventListener('click', function onClick() {
     if(res === "INITIAL NUMBER")
         res = 0;
     res++
-    result.innerHTML = res;
+    change(res);
+    resul.innerHTML = res;
 });
